bcff16b171dd97f598c91920f6130410049a5390
Anshu Kumar Singh <69234255+Ansh9728@users.noreply.github.com>
2023-07-03 00:45:35 +0530

    img file fix

--- main.py
+++ main.py
@@ -23,6 +23,24 @@ def text_preproces(text):
 
     return no_stopwords
 
+
+#adding the background image
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
+
 #Load the model
 
 with open('pipe.pkl', 'rb') as f:
@@ -45,7 +63,7 @@ if st.button('Predict'):
         prediction = model.predict(processed_input)[0]
 
         # Display the prediction
-        if prediction == 0:
+        if prediction == 'ham':
             st.success('NOT SPAM')
         else:
             st.error('SPAM.')
